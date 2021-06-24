6f79c55d19f3e30a0c7b3d3afa2e49f7b7b70682
aktrooper9 <48301195+aktrooper9@users.noreply.github.com>
2021-06-23 18:26:15 -0700

    finihsed format spaces

--- src/_00_Intro_To_String_Methods/_01_StringMethods.java
+++ src/_00_Intro_To_String_Methods/_01_StringMethods.java
@@ -34,13 +34,20 @@ public class _01_StringMethods {
 
     // Given Strings s1 and s2, return the longer String
     public static String longerString(String s1, String s2) {
-        return null;
+    	if(s1.length()>s2.length()) {
+    		return s1;
+    	}
+        return s2;
     }
 
     // If String s contains the word "underscores", change all of the spaces
     // to underscores
     public static String formatSpaces(String s) {
-        return null;
+    	String news = s;
+    if(s.contains("underscores")) {
+    	 news =s.replaceAll(" ", "_");
+    	}
+        return news;
     }
 
     // Return the name of the person whose LAST name would appear first if they
